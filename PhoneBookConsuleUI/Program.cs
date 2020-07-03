36d1243d021ac7d61863467829226b9bcd4783fa
Harley Rogers <harleydev512@gmail.com>
2020-07-03 16:52:01 -0500

    Add project files.

---
+++ PhoneBookConsuleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PhoneBookConsuleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
